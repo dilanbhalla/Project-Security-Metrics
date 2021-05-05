9aaee0e4155dd67f8373f10955130ca535f8ea40
Dilan <dbhalla21@berkeley.edu>
2021-05-05 01:09:22 -0700

    grafana: include non-starred packages in package search

--- src/grafana_variable.sql
+++ src/grafana_variable.sql
@@ -1,4 +1,8 @@
-SELECT p.package_url
-FROM package p left join metric m on p.id = m.package_id
-WHERE key like 'openssf.criticality.raw.stars'
-order by CAST(value as INT) desc
+SELECT t1.package_url
+from
+(SELECT distinct p.id, package_url FROM package p) as t1
+left join
+(SELECT p.id, m.value FROM package p left join metric m on p.id = m.package_id WHERE m.key like 'openssf.criticality.raw.stars') as t2
+on t1.id = t2.id
+order by case when t2.value is null then 1 else 0 end, CAST (t2.value as INT) desc
+
